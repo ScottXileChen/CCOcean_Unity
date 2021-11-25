015f8745b78809b8397988d6016db64a27482786
ScottXileChen <scottxilechen@gmail.com>
2021-11-25 14:30:44 -0800

    Scott_Chen
    - Added AudioManager.

---
+++ CCOcean/Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    private static AudioManager instance;
+    public static AudioManager Instance { get { return instance; } }
+
+    [SerializeField]
+    private Sound[] sounds;
+
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+        foreach (var s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
+    }
+
+    private void Start()
+    {
+        Play("Wind");
+    }
+
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning($"AudioManager -- Can't find {name} sound!");
+            return;
+        }
+        s.source.Play();
+    }
+}
